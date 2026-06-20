47869f97db27b6ca6e3888312778ace847edb9c2
Dhruv <dhruvkevadiya5@gmail.com>
2026-06-20 04:55:26 -0400

    N

--- src/main/java/com/jpmc/midascore/MidasCoreApplication.java
+++ src/main/java/com/jpmc/midascore/MidasCoreApplication.java
@@ -2,12 +2,22 @@ package com.jpmc.midascore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import java.util.Collections;
 
 @SpringBootApplication
 public class MidasCoreApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MidasCoreApplication.class, args);
+        SpringApplication app = new SpringApplication(MidasCoreApplication.class);
+        // Force the app to bind to port 33400 programmatically
+        app.setDefaultProperties(Collections.singletonMap("server.port", "33400"));
+        app.run(args);
     }
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
